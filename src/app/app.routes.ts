21e8f71dd3657c1afd30234081a9622efc9cf1d8
Fernando Vivanco <158092554+fjvivanco7@users.noreply.github.com>
2025-09-10 12:38:45 -0500

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { Login } from './login/login';
 import { Register } from './register/register';
 import { Profile } from './profile/profile';
 import { authGuard } from './auth-guard';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{ path: '', component: HomeComponent },
 	{ path: 'login', component: Login },
 	{ path: 'register', component: Register },
 	{ path: 'profile', component: Profile, canActivate: [authGuard] }
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TokenInterceptor } from './token.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Navbar } from './navbar/navbar';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, HomeComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   providers: [
